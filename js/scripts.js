9bfa367d26635840440de14f30c56e7085a48edc
Extreme Mission <37947057+extrememission@users.noreply.github.com>
2024-06-23 09:14:07 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -66,157 +66,157 @@ document.addEventListener("DOMContentLoaded", () => {
     { book: "3 John", chapters: 1 },
     { book: "Jude", chapters: 1 },
     { book: "Revelation", chapters: 22 }
-    ];
+  ];
 
-    const colors = ['#acac9c', '#948c8c', '#c7c4cc', '#bbbcb8', '#b4b3b4'];
+  const colors = ['#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff'];
 
-    function getRandomColor() {
-        return colors[Math.floor(Math.random() * colors.length)];
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  function createBookOptions() {
+    const booksContainer = document.getElementById('books');
+    booksContainer.innerHTML = '';
+    books.forEach((book) => {
+      const bookBox = document.createElement('div');
+      bookBox.classList.add('book-box', 'vignette');
+      bookBox.textContent = book.book;
+      const color = getRandomColor();
+      bookBox.style.backgroundColor = color;
+      bookBox.style.setProperty('--vignette-color', color);
+      bookBox.addEventListener('click', () => showChapters(book));
+      booksContainer.appendChild(bookBox);
+    });
+
+    const searchBox = document.getElementById('search-box');
+    booksContainer.appendChild(searchBox);
+    searchBox.addEventListener('click', showSearchModal);
+  }
+
+  function showChapters(book) {
+    const chaptersContainer = document.getElementById('chapters');
+    chaptersContainer.innerHTML = '';
+    for (let i = 1; i <= book.chapters; i++) {
+      const chapterBox = document.createElement('div');
+      chapterBox.classList.add('chapter-box', 'vignette');
+      chapterBox.textContent = `${book.book} ${i}`;
+      const color = getRandomColor();
+      chapterBox.style.backgroundColor = color;
+      chapterBox.style.setProperty('--vignette-color', color);
+      chapterBox.addEventListener('click', () => showVerses(book.book, i));
+      chaptersContainer.appendChild(chapterBox);
     }
 
-    function createBookOptions() {
-        const booksContainer = document.getElementById('books');
-        booksContainer.innerHTML = '';
-        books.forEach((book, index) => {
-            const bookBox = document.createElement('div');
-            bookBox.classList.add('book-box', 'vignette');
-            bookBox.textContent = book.book;
-            const color = getRandomColor();
-            bookBox.style.backgroundColor = color;
-            bookBox.style.setProperty('--vignette-color', color);
-            bookBox.addEventListener('click', () => showChapters(book));
-            booksContainer.appendChild(bookBox);
+    const reloadBox = document.createElement('div');
+    reloadBox.classList.add('reload-box');
+    reloadBox.textContent = 'Home';
+    reloadBox.addEventListener('click', () => location.reload());
+    chaptersContainer.appendChild(reloadBox);
+
+    chaptersContainer.scrollTop = 0;
+    document.getElementById('books').style.display = 'none';
+    chaptersContainer.style.display = 'block';
+  }
+
+ function showVerses(bookName, chapterNumber) {
+  const filePath = `data/${bookName}.json`;
+  fetch(filePath)
+    .then(response => response.json())
+    .then(data => {
+      const chapterData = data.chapters.find(chapter => chapter.chapter == chapterNumber);
+      const verses = chapterData.verses;
+
+      const versesContainer = document.getElementById('verses');
+      versesContainer.innerHTML = '';
+
+      verses.forEach((verse) => {
+        const verseBox = document.createElement('div');
+        verseBox.classList.add('verse-box', 'vignette');
+        verseBox.innerHTML = `${verse.text}<div style="text-align: center;">${bookName} ${chapterNumber}:${verse.verse}</div>`;
+        const color = getRandomColor();
+        verseBox.style.backgroundColor = color;
+        verseBox.style.setProperty('--vignette-color', color);
+
+        verseBox.addEventListener('click', () => {
+          const textToCopy = verseBox.textContent;
+          navigator.clipboard.writeText(textToCopy)
+            .then(() => alert('Verse copied to clipboard'))
+            .catch(err => console.error('Failed to copy text: ', err));
         });
 
-        // Add search box
-        const searchBox = document.getElementById('search-box');
-        booksContainer.appendChild(searchBox);
-        searchBox.addEventListener('click', showSearchModal);
-    }
+        versesContainer.appendChild(verseBox);
+      });
 
-    function showChapters(book) {
-        const chaptersContainer = document.getElementById('chapters');
-        chaptersContainer.innerHTML = '';
-        for (let i = 1; i <= book.chapters; i++) {
-            const chapterBox = document.createElement('div');
-            chapterBox.classList.add('chapter-box', 'vignette');
-            chapterBox.textContent = `${book.book} ${i}`;
-            const color = getRandomColor();
-            chapterBox.style.backgroundColor = color;
-            chapterBox.style.setProperty('--vignette-color', color);
-            chapterBox.addEventListener('click', () => showVerses(book.book, i));
-            chaptersContainer.appendChild(chapterBox);
-        }
-        chaptersContainer.scrollTop = 0;
-        document.getElementById('books').style.display = 'none';
-        chaptersContainer.style.display = 'block';
-    }
+      const reloadBox = document.createElement('div');
+      reloadBox.classList.add('reload-box');
+      reloadBox.textContent = 'RELOAD';
+      reloadBox.addEventListener('click', () => location.reload());
+      versesContainer.appendChild(reloadBox);
 
-    function showVerses(bookName, chapterNumber) {
-        const filePath = `data/${bookName}.json`;
-        fetch(filePath)
-            .then(response => response.json())
-            .then(data => {
-                const bookData = data.chapters.find(chapter => chapter.chapter == chapterNumber);
-                const versesContainer = document.getElementById('verses');
-                versesContainer.innerHTML = '';
-                bookData.verses.forEach((verse, index) => {
-                    const verseBox = document.createElement('div');
-                    verseBox.classList.add('verse-box', 'vignette');
-                    verseBox.textContent = `${verse.text}`;
-                    const color = getRandomColor();
-                    verseBox.style.backgroundColor = color;
-                    verseBox.style.setProperty('--vignette-color', color);
+      versesContainer.scrollTop = 0;
+      document.getElementById('chapters').style.display = 'none';
+      versesContainer.style.display = 'block';
+    })
+    .catch(error => console.error('Error fetching verses:', error));
+}
 
-                    const reference = document.createElement('div');
-                    reference.textContent = `(${bookName} ${chapterNumber}:${verse.verse})`;
-                    reference.classList.add('reference');
-                    verseBox.appendChild(reference);
+  function showSearchModal() {
+    document.getElementById('search-modal').style.display = 'flex';
+  }
 
-                    verseBox.addEventListener('click', () => copyVerseToPhotos(verseBox));
-                    versesContainer.appendChild(verseBox);
-                });
-                versesContainer.scrollTop = 0;
-                document.getElementById('chapters').style.display = 'none';
-                versesContainer.style.display = 'block';
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }
+  function closeSearchModal() {
+    document.getElementById('search-modal').style.display = 'none';
+  }
 
-    function copyVerseToPhotos(verseBox) {
-        html2canvas(verseBox).then(canvas => {
-            canvas.toBlob(blob => {
-                const verseText = verseBox.textContent.trim();
-                const referenceMatch = verseText.match(/\(.*:\d+\)/);
-                let reference = "";
+  function closeSearchModalAndReload() {
+    document.getElementById('search-modal').style.display = 'none';
+    location.reload();
+  }
 
-                if (referenceMatch) {
-                    reference = referenceMatch[0].slice(1, -1);
-                } else {
-                    console.warn("Verse reference not found in verseBox content. Using default filename.");
-                    reference = "verse_image.png";
-                }
+  function showBooksWindow() {
+    document.getElementById('books').style.display = 'block';
+    document.getElementById('chapters').style.display = 'none';
+    document.getElementById('verses').style.display = 'none';
+  }
 
-                const filename = `${reference}.png`;
+  function searchScriptures() {
+    const searchField = document.getElementById('search-field');
+    const searchQuery = searchField.value.toLowerCase();
+    const resultsContainer = document.getElementById('search-results');
+    resultsContainer.innerHTML = '';
 
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = filename;
-                a.style.display = 'none';
-                document.body.appendChild(a);
-                a.click();
-                document.body.removeChild(a);
-                URL.revokeObjectURL(url);
+    books.forEach(book => {
+      const filePath = `data/${book.book}.json`;
+      fetch(filePath)
+        .then(response => response.json())
+        .then(data => {
+          data.chapters.forEach(chapter => {
+            chapter.verses.forEach(verse => {
+              if (verse.text.toLowerCase().includes(searchQuery)) {
+                const resultBox = document.createElement('div');
+                resultBox.classList.add('verse-box', 'vignette');
+                resultBox.textContent = `${book.book} ${chapter.chapter}:${verse.verse} - ${verse.text}`;
+                const color = getRandomColor();
+                resultBox.style.backgroundColor = color;
+                resultBox.style.setProperty('--vignette-color', color);
+                resultBox.addEventListener('click', () => { showVerses(book.book, chapter.chapter); closeSearchModal(); });
+                resultsContainer.appendChild(resultBox);
+              }
             });
-        });
-    }
+          });
+        })
+        .catch(error => console.error('Error searching verses:', error));
+    });
 
-    function showSearchModal() {
-        document.getElementById('search-modal').style.display = 'flex';
-        document.getElementById('close-modal-btn').addEventListener('click', hideSearchModal);
-    }
+    // Hide unnecessary elements
+    document.getElementById('books').style.display = 'none';
+    document.getElementById('chapters').style.display = 'none';
+    document.getElementById('verses').style.display = 'none';
+    document.getElementById('search-box').style.display = 'none';
+  }
 
-    function hideSearchModal() {
-        document.getElementById('search-modal').style.display = 'none';
-    }
+  document.getElementById('close-modal-btn').addEventListener('click', closeSearchModalAndReload);
+  document.querySelector('.begin-search-box').addEventListener('click', searchScriptures);
 
-    function performSearch() {
-        const searchTerm = document.getElementById('search-field').value.toLowerCase();
-        const resultsContainer = document.getElementById('search-results');
-        resultsContainer.innerHTML = '';
-
-        books.forEach(book => {
-            const filePath = `data/${book.book}.json`;
-            fetch(filePath)
-                .then(response => response.json())
-                .then(data => {
-                    data.chapters.forEach(chapter => {
-                        chapter.verses.forEach((verse, index) => {
-                            if (verse.text.toLowerCase().includes(searchTerm)) {
-                                const verseBox = document.createElement('div');
-                                verseBox.classList.add('verse-box', 'vignette');
-                                verseBox.textContent = `${verse.text}`;
-                                const color = getRandomColor();
-                                verseBox.style.backgroundColor = color;
-                                verseBox.style.setProperty('--vignette-color', color);
-
-                                const reference = document.createElement('div');
-                                reference.textContent = `(${book.book} ${chapter.chapter}:${verse.verse})`;
-                                reference.classList.add('reference');
-                                verseBox.appendChild(reference);
-
-                                verseBox.addEventListener('click', () => copyVerseToPhotos(verseBox));
-                                resultsContainer.appendChild(verseBox);
-                            }
-                        });
-                    });
-                })
-                .catch(error => console.error('Error fetching data:', error));
-        });
-    }
-
-    document.querySelector('.begin-search-box').addEventListener('click', performSearch);
-
-    createBookOptions();
+  createBookOptions();
 });
